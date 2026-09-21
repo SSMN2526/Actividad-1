385109272d4d51174501b3eae778cee61b7335d0
Sergio Sanchez Moreno <sergio16x09@gmail.com>
2026-09-21 19:41:25 +0200

    Funcionalidad 1 D1

---
+++ Actividad 1/Actividad 1/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Actividad_1
+{
+    public class Persona
+    {
+        public string nombre;
+        public int edad;
+        public string Dni;
+    }
+}
--- Actividad 1/Actividad 1/obj/Debug/net10.0/Actividad 1.AssemblyInfo.cs
+++ Actividad 1/Actividad 1/obj/Debug/net10.0/Actividad 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Actividad 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eec2409209e88eb155015f03bf66f72048a1cef0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Actividad 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Actividad 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
